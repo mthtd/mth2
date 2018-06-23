70c7a799cd5d07e519daa8321b79a4b682b48102
mthtd <40235124+mthtd@users.noreply.github.com>
2018-06-23 13:46:41 +0800

    ---
    ----

---
+++ 财经新闻_files/changeimg.js
@@ -0,0 +1,20 @@
+﻿
+　　var totalAds = 3;//图片数量（数组大小）
+
+　　var nameAds = new Array(totalAds);
+
+　　var linkAds = new Array(totalAds);
+
+　　var imgAds = new Array(totalAds);
+
+　　nameAds[0]="DELL";linkAds[0]="#";imgAds[0]="images/1.jpg";
+　　nameAds[1]="DELL";linkAds[1]="#";imgAds[1]="images/2.jpg";
+　　nameAds[2]="DELL";linkAds[2]="#";imgAds[2]="images/3.jpg";
+    
+
+
+
+	
+　　var numAds = Math.floor(Math.random() * totalAds);
+
+
